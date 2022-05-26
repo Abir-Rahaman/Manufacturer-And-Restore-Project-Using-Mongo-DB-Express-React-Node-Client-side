cd3e8768a3c8fa553295ec840a36be8611607966
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-05-26 18:25:33 +0600

    quantity update done

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -8,6 +8,7 @@ import { toast } from 'react-toastify';
 
 const Booking = () => {
     const {id} = useParams();
+    const [isReload,setReload] = useState(false)
     const [ tools, setTools] = useState({});
     const {name,price,quantity,minimum,_id} = tools;
     useEffect(()=>{
@@ -15,20 +16,25 @@ const Booking = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setTools(data));
-    },[]) 
+    },[id,isReload]) 
     // console.log(computer);
     const [user] = useAuthState(auth)
     const handleBook = e =>{
+        const need = e.target.need.value;
         e.preventDefault();
         const booking = {
             productId:_id,
             productName:name,
+            need,
             bookingEmail:user.email,
             UserName:user.displayName,
             productPrice:price,
             productQuantity:quantity,
-            minimumQuantity:minimum
+            minimumQuantity:minimum,
+         
+            
         }
+        
         fetch('http://localhost:4000/booking', {
             method: 'POST',
             headers:{
@@ -42,8 +48,41 @@ const Booking = () => {
                 toast.success("done")
                  
             });
+
+         
+                const newQuantity = parseInt(quantity)-parseInt(need)
+                console.log(newQuantity);
+                const updateQuantity= {newQuantity};
+                if(!quantity){
+                    alert("added")
+                }
+                else{
+                    const url=`http://localhost:4000/tools/${id}`
+                    fetch(url,{
+                        method:"PUT",
+                        headers: {
+                            'Content-Type': 'application/json',
+                          },
+                          body: JSON.stringify(updateQuantity),
+        
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        setReload(!isReload);
+                    console.log('Success:', data);
+                    })
+                }
+        
+            
+
+               
+               
+        
+            
     
     }
+
+    
     return (
         <div className='w-full' style={{
             background: `url(${bgsell})`,
@@ -72,8 +111,11 @@ const Booking = () => {
                     <span className="label-text font-bold text-primary">Product Price</span>
                 </label>
                     <input type="text" name='bookingPrice' disabled readOnly value={price|| "not found"} className="input input-bordered input-info w-full max-w-xs rounded-full " />
-
-                    <input type="submit" value='purchase' placeholder="Type here" className="btn btn-primary btn-wide mt-3 text-white rounded-full " /> 
+                    <label className="label">
+                    <span className="label-text font-bold text-primary">How Much You Want But You Can't Order less than {minimum} Or more Than {quantity} </span>
+                </label>
+                    <input type="text" name='need'  className="input input-bordered input-info w-full max-w-xs rounded-full " />
+                    <input  type="submit" value='purchase' placeholder="Type here" className="btn btn-primary btn-wide mt-3 text-white rounded-full " /> 
                     </form>
             
             </div>
