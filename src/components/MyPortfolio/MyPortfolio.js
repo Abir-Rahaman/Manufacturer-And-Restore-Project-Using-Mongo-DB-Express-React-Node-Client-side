443e688a49dcef07ceb4ec0c6d23addbf8d3a13a
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-05-26 23:28:24 +0600

    done

--- src/components/MyPortfolio/MyPortfolio.js
+++ src/components/MyPortfolio/MyPortfolio.js
@@ -14,9 +14,16 @@ const MyPortfolio = () => {
                    <div className="">
                        <a href="https://mail.google.com/mail/u/0/#inbox"> abirahaman.cse@gmail.com</a> <br />
                        <a href="https://mail.google.com/mail/u/1/#inbox">abir15-2520@diu.edu.bd</a>
-
                    </div>
                 </div>
+
+               <div className="mt-10">
+               <h1 className='text-2xl text-primary font-bold text-center my-5'> My 3 own Fokira Projects </h1>
+                <a className='text-xl font-bold text-center ml-14' href="https://cool-boba-626a6a.netlify.app/"> My Portfolio Website  </a> <br />
+                <a className='text-xl font-bold text-center ml-14' href="https://wondrous-brigadeiros-7e1854.netlify.app/">  Blog Website </a> <br />
+                <a className='text-xl font-bold text-center ml-14' href="https://tangerine-crisp-412e96.netlify.app/"> Teaching Web site  </a> <br />
+               </div>
+                
                 </div>
         </div>
     );
